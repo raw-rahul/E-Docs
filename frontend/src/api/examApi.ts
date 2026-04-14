47457e79be25280bad8229552213f22945473d7b
Rahul Kumar <78088295+raw-rahul@users.noreply.github.com>
2026-04-15 02:43:48 +0530

    Update examApi.ts

--- frontend/src/api/examApi.ts
+++ frontend/src/api/examApi.ts
@@ -30,7 +30,8 @@ import axios from "axios";
 import { saveAs } from "file-saver";
 
 // const API_URL = "http://127.0.0.1:8000";
-const API_URL = "http://localhost:8080/api";
+// const API_URL = "http://localhost:8080/api";
+const API_URL = "https://springboot-backend-ljy3.onrender.com/api";
 
 // 🔍 Fetch exam
 export const fetchExam = async (query: string) => {
